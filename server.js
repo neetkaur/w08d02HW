cf70e1acc9bce0cbdf8d3830b38bfbf71e1ebda5
Neet <navneetkaur3@gmail.com>
2021-06-12 03:38:07 -0500

    "Magic 8 Ball express application created."

--- server.js
+++ server.js
@@ -12,7 +12,17 @@ app.get('/greeting/:name',(req,res)=>{
 
 app.get('/tip/:total/:percentage',(req,res)=>{
   res.send(`Your tip amount is ${req.params.total/100*req.params.percentage}`)
-
 })
 
+magicAnswer = ["It is certain", "It is decidedly so", "Without a doubt",
+"Yes definitely","You may rely on it", "As I see it yes", "Most likely",
+"Outlook good","Yes", "Signs point to yes", "Reply hazy try again",
+"Ask again later","Better not tell you now", "Cannot predict now",
+"Concentrate and ask again","Don't count on it", "My reply is no",
+"My sources say no","Outlook not so good", "Very doubtful"]
+
+app.get('/magic/Will%20I%20Be%20A%20Millionaire',(req,res)=>{
+  res.send(`Will I be a Millionaire \n
+    <h1> ${magicAnswer[magicAnswer.length * Math.random() | 0]}</h1>`)
+})
 app.listen(PORT,()=>console.log("Listening @ 8080"))
