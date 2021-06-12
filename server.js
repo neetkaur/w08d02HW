b8ee1af7418b0bcd56e7bee36000852d2ed4c6d5
Neet <navneetkaur3@gmail.com>
2021-06-12 04:20:14 -0500

    Take one Down and Pass it Around

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const PORT = 8080
-
+//Greeting
 app.get('/greeting',(req,res)=> {
   res.send("Hello Stranger")
 })
@@ -10,10 +10,12 @@ app.get('/greeting/:name',(req,res)=>{
   res.send(`Wow! Hello there, ${req.params.name}`)
 })
 
+//Tip
 app.get('/tip/:total/:percentage',(req,res)=>{
   res.send(`Your tip amount is ${req.params.total/100*req.params.percentage}`)
 })
 
+//MagicAnswer
 magicAnswer = ["It is certain", "It is decidedly so", "Without a doubt",
 "Yes definitely","You may rely on it", "As I see it yes", "Most likely",
 "Outlook good","Yes", "Signs point to yes", "Reply hazy try again",
@@ -25,4 +27,30 @@ app.get('/magic/Will%20I%20Be%20A%20Millionaire',(req,res)=>{
   res.send(`Will I be a Millionaire \n
     <h1> ${magicAnswer[magicAnswer.length * Math.random() | 0]}</h1>`)
 })
+
+
+
+let numberOfBugs = 99
+
+//Homepage statement
+app.get('/bugs',(req,res)=>{
+  res.send(`<h1> ${numberOfBugs} bugs in the code </h1> \n
+    <a href = "/bugs/98">Take one down and pass it around</a>`)
+})
+//Start Reducing
+
+app.get('/bugs/:numberOfBugs',(req,res)=>{
+  while(numberOfBugs !==0){
+  res.send(`<h1> ${numberOfBugs} bugs in the code </h1> \n
+    <a href = "/bugs/:numberOfBugs">Take one down and pass it around</a>`)
+    if(Math.round(Math.random() * 10)< 5){
+      numberOfBugs +=100
+    }
+    else{numberOfBugs--}
+  }
+    numberOfBugs = 99
+})
+
+
+
 app.listen(PORT,()=>console.log("Listening @ 8080"))
